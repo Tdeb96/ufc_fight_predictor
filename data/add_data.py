5a5030680dc1c7907ef428430956686859b6a849
thijs <thijs.debruin@rystadenergy.com>
2022-05-18 12:42:47 +0200

    initial work on dockerizing data dump

---
+++ data/add_data.py
@@ -0,0 +1,28 @@
+from sqlalchemy import create_engine
+from sqlalchemy.engine.base import Engine
+import pandas as pd 
+import os
+import logging
+import csv
+
+def get_db_engine(
+    username: str,
+    password: str,
+    protocol: str = "postgresql",
+    server: str = "localhost",
+    port: int = 5432,
+    dbname: str = "ufc",
+) -> Engine:
+    engine = create_engine(
+        f"{protocol}://" f"{username}:" f"{password}@" f"{server}:" f"{port}/" f"{dbname}"
+    )
+    return engine
+
+engine = get_db_engine(os.environ.get("POSTGRES_USERNAME"), os.environ.get("POSTGRES_PASSWORD"))
+bouts = pd.read_csv('data/bouts.csv')
+fighters = pd.read_csv('data/fighters.csv', quoting=csv.QUOTE_NONE)
+
+logging.info('Uploading bouts and fighters csv files to postgres server')
+with engine.connect() as conn:
+    bouts.to_sql('bouts', conn, 'ufc', if_exists='replace')
+    fighters.to_sql('bouts', conn, 'ufc', if_exists='replace')
