a297f56d877a4d3b575cad177a8d12e40faed4f3
Thijs de bruin <thijs.debruin@rystadenergy.com>
2023-05-10 16:59:37 +0200

    testing

---
+++ test.py
@@ -0,0 +1,6 @@
+import os
+
+def very_important_function(template: str, *variables, file: os.PathLike, engine: str, header: bool = True, debug: bool = False):
+    """Applies `variables` to the `template` and writes to `file`."""
+    with open(file, 'w') as f:
+        ...
