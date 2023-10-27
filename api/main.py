e0cf0ce90a6105dcb7bc1a7b4425ac15c997c4ac
Thijs de Bruin <thijs.debruin@rystadenergy.com>
2023-10-27 23:01:26 +0200

    initial working version api and web app

---
+++ api/main.py
@@ -0,0 +1,35 @@
+import json
+
+import pandas as pd
+from fastapi import FastAPI
+from xgboost import XGBClassifier
+
+from ufc_fight_predictor import DataProcessor
+
+app = FastAPI()
+data_processor = DataProcessor(username="postgres", password="postgres")
+
+clf = XGBClassifier()
+clf.load_model("api/models/model.xgb")
+
+
+@app.get("/")
+def read_root():
+    return {"message": "Welcome to the API"}
+
+
+@app.get("/predict")
+def predict(fighter1: str, fighter2: str):
+    difference_df = data_processor.calculate_differences_on_inference(
+        fighter1, fighter2
+    )
+    prediction = clf.predict_proba(difference_df)
+
+    output_dict = {
+        "win_prob": float(prediction[0][1]),
+        "loss_prob": float(prediction[0][0]),
+    }
+
+    # Convert to json
+    output_json = json.dumps(output_dict)
+    return output_json
---
+++ web_app/streamlit_app.py
@@ -0,0 +1,42 @@
+import json
+
+import requests
+import streamlit as st
+
+from ufc_fight_predictor import DataProcessor
+
+# API URL
+url = "http://api:8000/predict"
+
+# Initialize data generator and load data
+data_generator = DataProcessor(username="postgres", password="postgres")
+ufc_fighters = data_generator.load_fighters()
+
+# Fetch the names of all fighters
+fighter_names = ufc_fighters["fighter_name"]
+
+# Main title
+st.title("UFC Fight Predictor")
+
+# User Inputs
+st.markdown("### Select Fighters")
+col1, col2 = st.columns(2)
+
+with col1:
+    selected_fighter1 = st.selectbox("Fighter 1", fighter_names)
+
+with col2:
+    selected_fighter2 = st.selectbox("Fighter 2", fighter_names)
+
+# Making a GET request to the FastAPI server running on localhost
+if st.button("Predict"):
+    response = requests.get(
+        url, params={"fighter1": selected_fighter1, "fighter2": selected_fighter2}
+    )
+    if response.status_code == 200:
+        result = response.json()
+        result = json.loads(result)
+        st.write(f"Win Probability: {result['win_prob']}")
+        st.write(f"Loss Probability: {result['loss_prob']}")
+    else:
+        st.write("Could not get a valid response from the API.")
